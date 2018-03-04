171b3792dccd777f5b5d0834f0519605f4210911
Mohit Sharma <msvbs98@gmail.com>
2018-03-04 20:02:48 +0530

    Open Links in external browser

--- app/renderer/js/main.js
+++ app/renderer/js/main.js
@@ -1,4 +1,5 @@
 onload = function () {
+	const {shell} = require('electron');
 	const webview = document.querySelector('webview');
 	const loading = document.querySelector('#loader');
 
@@ -9,7 +10,14 @@ onload = function () {
 	function onStartLoad() {
 		loading.classList.remove('hide');
 	}
-
+	
+	webview.addEventListener('new-window', e => {
+		const protocol = require('url').parse(e.url).protocol;
+		if (protocol === 'http:' || protocol === 'https:') {
+			shell.openExternal(e.url);
+		}
+	});
+	
 	webview.addEventListener('did-stop-loading', onStopLoad);
 	webview.addEventListener('did-start-loading', onStartLoad);
 };
